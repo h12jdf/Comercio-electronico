e78411d0be1a10264102aab1972b60af8e9162ed
Luis Montoya Moreno <63527526+SupplelyBasil36@users.noreply.github.com>
2024-11-28 22:37:43 -0600

    Update login_trabajador.js

--- js/login_trabajador.js
+++ js/login_trabajador.js
@@ -14,7 +14,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
     const data = await response.json();
     alert(`Inicio de sesión exitoso: ${data.user.Nombre}`);
     localStorage.setItem("userData", JSON.stringify(data.user));
-    window.location.href = "Comercio-electronico/index_trabajador.html";
+    window.location.href = "/index_trabajador.html";
   } catch (error) {
     alert("Hubo un error: " + error.message);
   }
