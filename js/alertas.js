698994a4fcb0606992893d1e7551ff60b2d4df5c
h12jdf <58224128+h12jdf@users.noreply.github.com>
2024-12-01 13:01:26 -0600

    Add files via upload

---
+++ js/alertas.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+function mostrarAlerta() {
+  document.getElementById('customAlert').style.display = 'block';
+  document.getElementById('overlay').style.display = 'block';
+}
+
+function cerrarAlerta() {
+  document.getElementById('customAlert').style.display = 'none';
+  document.getElementById('overlay').style.display = 'none';
+}
+
+function mostrarAlerta1() {
+  document.getElementById('customAlert1').style.display = 'block';
+  document.getElementById('overlay1').style.display = 'block';
+}
+
+function cerrarAlerta1() {
+  document.getElementById('customAlert1').style.display = 'none';
+  document.getElementById('overlay1').style.display = 'none';
+}
+
+function mostrarAlerta2() {
+  document.getElementById('customAlert1').style.display = 'block';
+  document.getElementById('overlay1').style.display = 'block';
+}
+
+function cerrarAlerta2() {
+  document.getElementById('customAlert1').style.display = 'none';
+  document.getElementById('overlay1').style.display = 'none';
+}
