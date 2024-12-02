81914825b15a162ec45f0ddc45a0e9591991b9e4
SupplelyBasil36 <luismontoya1714@gmail.com>
2024-12-01 23:52:42 -0600

    update detalles_producto

--- js/validador.js
+++ js/validador.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (userData) {
     // Muestra los datos del usuario en la página
     document.getElementById("username").innerText = userData.Nombre;
+    document.getElementById("location-text").innerText = userData.Ciudad
   } else {
     // Si no hay datos, redirige al login
     window.location.href = window.location.origin + "/Comercio-electronico/login.html";
